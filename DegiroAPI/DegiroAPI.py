fe07eb7e99c86d06046d76f0a31db2a4c1cb1532
Louvivien <43605778+Louvivien@users.noreply.github.com>
2020-04-05 18:49:51 +0200

    Update DegiroAPI.py

--- DegiroAPI/DegiroAPI.py
+++ DegiroAPI/DegiroAPI.py
@@ -306,7 +306,6 @@ class DegiroAPI:
         """
         
         res = self.check_order(order)
-        order_status = res.get("status")
         if res['data']['confirmationId'] != '':
             confirm_id = res['data']['confirmationId']
             confirmation_res = self.confirm_order(order, str(confirm_id))
